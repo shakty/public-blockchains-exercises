29ce9a5899bc91960eb4ec73c1a40b65b4000469
Stefano Balietti <futur.dorko@gmail.com>
2025-03-18 12:23:30 +0100

    gas and resubmit

--- 3_EtherJS/4b_signer_gas.js
+++ 3_EtherJS/4b_signer_gas.js
@@ -4,6 +4,9 @@ const path = require('path');
 // Ethers JS: Signers: Gas and Transactions.
 ////////////////////////////////////////////
 
+
+// These are bonus exercises!
+
 // Exercise 1. Meddling with Gas.
 /////////////////////////////////
 
@@ -94,55 +97,3 @@ const sendCheaperTransaction = async () => {
 };
 
 // sendCheaperTransaction();
-
-
-
-// Exercise 6. Resubmitting a transaction.
-//////////////////////////////////////////
-
-// Let's get a transaction pending in the mempool for a long time. It is 
-// quite difficult to do it with Ethers.JS because it prevents to send
-// transactions with too low maxFeePerGas. You could try setting a ver low
-// `maxPriorityFeePerGas` but some miner might pick up your transaction 
-// nonetheless (btw the bare minimum you should tip the miner is 1 wei, 
-// but around 2 gwei is usually considered a safe choice).
-
-// Let's use Metamask. Make sure you have the right options enabled: go to Settings/Advanced and tick 
-// "Advanced gas controls" and "Customize transaction nonce".
-
-// So let's submit a transaction with Metamask, setting a very low
-// `maxFeePerGas`. As you do it, note the nonce for this transaction 
-// (you may also get the nonce programmatically or from Etherscan).
-
-// a. Check that the Metamask transaction is pending. Wait a bit...
-
-// b. Now speed up that transaction. Send another transaction with the _same_ 
-// nonce, but with a more reasonable `maxFeePerGas`. Check that the transaction
-// goes through.
-
-// Hint: if you don't know the nonce, `getNonce` will tell you the _next_ one.
-// Hint2: if there is a transaction in the mempool, `getNonce` will give 
-// give the current nonce (same as transaction in the mempool). Try "pending"
-// as input paramter if you need the _next_ one. 
-// Hint3: if you don't know what a reasonable `maxFeePerGas` is, you can 
-// get an idea calling `getFeeData()`.
-
-const resubmitTransaction = async () => {
-
-    // Your Code here!
-
-};
-
-resubmitTransaction();
-
-
-// c. Bonus. Repeat a+c., but this time cancel the transaction. How? Send a
-// transaction with the same nonce with zero value and recipient address
-// equal to sender address.
-
-const cancelTransaction = async () => {
-
-    // Your Code here!
-};
-
-cancelTransaction();
