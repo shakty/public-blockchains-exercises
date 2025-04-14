f70da9a437f7b83afe6c27aa76cf938c1da7bf77
Priyanka Gangavkar <pgangavkar@gmail.com>
2025-04-14 13:40:25 +0200

    submitting assigment 3

--- 4_Hardhat/ignition/modules/CensorableToken.js
+++ 4_Hardhat/ignition/modules/CensorableToken.js
@@ -1,11 +1,13 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules")
+require('dotenv').config()
 
 module.exports = buildModule("CensorableTokenModule", (m) => {
-  const name = "CensoredToken"
+  const name = "CensorableToken"
   const symbol = "CST"
   const initialSupply = 25
-  const owner = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
-  const validator = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+  const owner = process.env.METAMASK_1_ADDRESS
+  const validatorLocal = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+  const validator = "0x8452E41BA34aC00458B70539264776b2a379448f"
 
   const con = m.contract('CensorableToken', [name, symbol, initialSupply, owner, validator])
 
